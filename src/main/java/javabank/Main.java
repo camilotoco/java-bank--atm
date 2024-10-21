232383e8f77b1f82d0cc6e06e9e2703882cff326
camilotoco <camilotoco@gmail.com>
2024-10-21 10:44:45 -0500

    se agrego funcionalidad

--- src/main/java/javabank/Main.java
+++ src/main/java/javabank/Main.java
@@ -2,6 +2,6 @@ package javabank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo cambiado por git!");
     }
 }
